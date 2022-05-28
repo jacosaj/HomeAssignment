8c4ff694ee973bf597592258bb0176d03a4b1333
Jacek Sajnog <jacek.sajnog@hotmail.com>
2022-05-28 13:57:22 +0200

    Add Requesting word

--- src/com/request/HTTPRequest.java
+++ src/com/request/HTTPRequest.java
@@ -1,7 +1,27 @@
 package com.request;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
 
 public class HTTPRequest {
-    public static void main(String[] args) {
-        System.out.println("Request");
+        private static final URI WORDS_URI =
+                URI.create("https://random-words-api.vercel.app/word");
+
+        public static void main(String[] args) throws Exception {
+            System.out.println(getRandomWords());
+        }
+
+        private static String getRandomWords() throws Exception {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(WORDS_URI)
+                    .GET()
+                    .build();
+
+            return client.send(request, ofString())
+                    .body();
+        }
     }
-}
