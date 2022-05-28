64ad780f917b60616db1e1b9ef68da0a29c14943
Jacek Sajnog <jacek.sajnog@hotmail.com>
2022-05-28 23:25:47 +0200

    Full project

--- src/Main.java
+++ src/Main.java
@@ -40,12 +40,10 @@ public class Main {
                 .body();
     }
     public static void main(String[] args) throws Exception {
-        //Scanner sc = new Scanner(System.in);
-        System.out.print("Please, enter any integer number from 5 to 20: ");
-        int i = 6;//sc.nextInt();
-        if (i>= 5 && i <=20) {
-            System.out.println("You entered this integer number: " + i);
-        } else {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter any integer number from 5 to 20: ");
+        int i = sc.nextInt();
+        if (i < 5 || i > 20) {
             System.out.println("You entered wrong integer number");
             return;
         }
@@ -58,16 +56,32 @@ public class Main {
             do {
                 String randomWord = getRandomWord();
                 JSONArray response = new JSONArray(randomWord);
-                word = ((JSONObject)response.get(0)).getString("word");
+                word = response.getJSONObject(0).getString("word");
             } while (randomWords.contains(word));
             randomWords.add(word);
             count++;
         }
+        System.out.println("Those are your random words:");
         System.out.println(randomWords);
+        System.out.println();
+
         for (String randomWord: randomWords){
-
+            System.out.println("Recording info from: " + randomWord);
+            String songFromWord = getSongFromWord(randomWord);
+            JSONObject songResponse = new JSONObject(songFromWord);
+            JSONArray recordings = songResponse.getJSONArray("recordings");
+            if (!recordings.isEmpty()) {
+                JSONObject recording = recordings.getJSONObject(0);
+                String artist = recording.getJSONArray("artist-credit").getJSONObject(0).getString("name");
+                String album = recording.getJSONArray("releases").getJSONObject(0).getString("title");
+                String title = recording.getString("title");
+                System.out.println("Title: " + title);
+                System.out.println("Album: " + album);
+                System.out.println("Artist: " + artist);
+            } else {
+                System.out.println("No recording found!");
+            }
+            System.out.println();
         }
-        System.out.println(getSongFromWord(randomWords.get(0)));
-
     }
 }
