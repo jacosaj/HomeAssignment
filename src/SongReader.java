bc56e91a1a8d7b793aa712bfd70cbce3670dc1d4
Jacek Sajnog <jacek.sajnog@hotmail.com>
2022-05-29 22:29:41 +0200

    Add multiple words to query

--- src/SongReader.java
+++ src/SongReader.java
@@ -1,13 +1,22 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.util.List;
 
 import static java.net.http.HttpResponse.BodyHandlers.ofString;
 
 public class SongReader {
-    public static String getSongFromWord(String word) throws Exception {
+    public static String getSongFromWord(List<String> words) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
-        URI songUri = URI.create("https://musicbrainz.org/ws/2/recording/?fmt=json&query=recording:" + word);
+        StringBuilder combinedWords = new StringBuilder();
+        for (int i = 0; i < words.size(); i++) {
+            if (i == words.size() - 1){
+                combinedWords.append(words.get(i));
+            } else {
+                combinedWords.append(words.get(i)).append(" OR ");
+            }
+        }
+        URI songUri = URI.create("https://musicbrainz.org/ws/2/recording/?fmt=json&query=recording:" + combinedWords);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(songUri)
                 .GET()
