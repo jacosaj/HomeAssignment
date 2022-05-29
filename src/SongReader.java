19ea04cada27320167f509c7e3973cbc984ebba2
Jacek Sajnog <jacek.sajnog@hotmail.com>
2022-05-29 22:58:04 +0200

    Old Version

--- src/SongReader.java
+++ src/SongReader.java
@@ -1,22 +1,14 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.util.List;
 
 import static java.net.http.HttpResponse.BodyHandlers.ofString;
 
 public class SongReader {
-    public static String getSongFromWord(List<String> words) throws Exception {
+    public static String getSongFromWord(String word) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
-        StringBuilder combinedWords = new StringBuilder();
-        for (int i = 0; i < words.size(); i++) {
-            if (i == words.size() - 1){
-                combinedWords.append(words.get(i));
-            } else {
-                combinedWords.append(words.get(i)).append(" OR ");
-            }
-        }
-        URI songUri = URI.create("https://musicbrainz.org/ws/2/recording/?fmt=json&query=recording:" + combinedWords);
+
+        URI songUri = URI.create("https://musicbrainz.org/ws/2/recording/?fmt=json&query=recording:" + word);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(songUri)
                 .GET()
